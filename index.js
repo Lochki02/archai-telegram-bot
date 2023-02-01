5242251b791c02c32e46f22bcd5c66310ed2853b
IoSonoN <crudocommunityofficial@gmail.com>
2023-02-01 19:32:28 +0100

    fixed ask command

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-//const { Telegraf } = require('telegraf');
-const { Composer } = require('micro-bot')
+const { Telegraf } = require('telegraf');
+//const { Composer } = require('micro-bot')
 const { BOT_TOKEN } = process.env
-//const bot = new Telegraf(BOT_TOKEN);
-const bot = new Composer
+const bot = new Telegraf(BOT_TOKEN);
+//const bot = new Composer
+const dexToolsLink = "https://www.dextools.io/app/en/ether/pair-explorer/0x94ce5a0677e32584a672fa28a6dcb63b53b8196f"
+const uniswapLink = "https://app.uniswap.org/#/swap?inputCurrency=0x5c8190b76e90b4dd0702740cf6eb0f7ee01ab5e9&outputCurrency=ETH"
 
 const { filterSentence, queryKeywords, queryString, askChatGPT, structureDoc, sendToDb } = require("./utils")
 
@@ -11,13 +13,13 @@ class TemporaryObject{
     question = ""
     answer = ""
     username = ""
-    chat = 0
+    user = 0
 
-    constructor(question, answer, username, chat){
+    constructor(question, answer, username, user){
         this.question = question
         this.answer = answer
         this.username = username
-        this.chat = chat
+        this.user = user
     }
 }
 
@@ -25,78 +27,91 @@ let tempArray = []
 
 bot.command('start', async (ctx) => {
     ctx.telegram.sendMessage(ctx.chat.id, `
-    Hi, ${ctx.update.message.from.username ? ctx.update.message.from.username : "User"}!
-
+    Hi, *${ctx.update.message.from.username ? ctx.update.message.from.username : "User"}*!
+    
     I'm an AI bot, powered by @AiArchiveERC.
     Join in on archiving the AI mind.
-
-
+    
+    
     aioverflow.app X CodeAI.
-
-
+    
+    
     Commands:
     /commands - Display Code AI's commands.
     /ask - Ask Code AI a programming related question.
     /archive - Search content into Archive AI.
-
-    Please keep in mind this is a beta, so if you have any problem report to @AiArchiveERC
-    `)
+    
+Please keep in mind this is a beta, so if you have any problem report to @AiArchiveERC
+    `,{
+        parse_mode: "Markdown"
+    })
 });
 
 bot.command('commands', async (ctx) => {
-    ctx.deleteMessage(ctx.update.message.message_id);
-
     ctx.telegram.sendMessage(ctx.chat.id, `
-        Commands:
-            /archive - Fetch data into the ArchAI database using a question.
-            /ask - Ask ArchAI a question.
-        `
+Commands:\n
+*/archive* - _Fetch data into the ArchAI database using a question._
+*/ask* - _Ask ArchAI a question._
+        `,
+        {
+            parse_mode: "Markdown"
+        }
     );
 });
 
 bot.command('ask', async (ctx) => {
     const chatID = ctx.chat.id
-    console.log(chatID)
 
     try{
         const tempReply = await ctx.telegram.sendMessage(chatID, "Processing your question...")
 
         const question = ctx.message.text.substring(5);
 
-        let answer = await askChatGPT(question, "text-davinci-003")
+        if(question != "") {
+            let answer = await askChatGPT(question, "text-davinci-003")
 
-        if(!answer){
-            ctx.telegram.sendMessage(chatID, "Something went wrong")
-        }
-        else{
-            ctx.telegram.deleteMessage(chatID, tempReply.message_id)
-            let tempObj = new TemporaryObject(question, answer, ctx.from.username, chatID)
+            if(!answer){
+                ctx.telegram.sendMessage(chatID, "Something went wrong")
+            }
+            else{
+                ctx.telegram.deleteMessage(chatID, tempReply.message_id)
+                let tempObj = new TemporaryObject(question, answer, ctx.from.username, ctx.from.id)
 
-            tempArray = tempArray.filter((obj) => obj.chat != chatID)
-            tempArray.push(tempObj)
+                tempArray = tempArray.filter((obj) => obj.user != ctx.from.id)
+                tempArray.push(tempObj)
 
-            ctx.telegram.sendMessage(chatID, answer)
-            ctx.telegram.sendMessage(chatID,`
-            If you are happy with the answer recieved from ArchAIBot please consider helping us by requesting a push to add this data to the database
-                `,
-                {
-                    reply_markup:{
-                        inline_keyboard:[
-                            [
-                                {
-                                    text: "Push",
-                                    callback_data: "push_data"
-                                },
-                                {
-                                    text: "Don't",
-                                    callback_data: "reset"
-                                }
-                            ]
-                        ]
+                ctx.telegram.sendMessage(
+                    chatID, 
+                    `*${ctx.from.username ? ctx.from.username : "User"}:* /ask ${question}\n\n*Answer*\n${answer}\n\n[ArchAI](https://AiArchive.io) | [Chart](${dexToolsLink}) | [Buy](${uniswapLink})`,
+                    { 
+                        parse_mode: "Markdown", 
+                        disable_web_page_preview: true 
                     }
-                }
-            )
+                ).then((msg) =>{
+                    ctx.telegram.sendMessage(chatID,`
+                    If you are happy with the answer recieved from ArchAIBot please consider helping us by requesting a push to add this data to the database
+                        `,
+                        {
+                            reply_markup:{
+                                inline_keyboard:[
+                                    [
+                                        {
+                                            text: "Push",
+                                            callback_data: "push_data"
+                                        },
+                                        {
+                                            text: "Don't",
+                                            callback_data: "reset"
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    )
+                })
+            }
         }
+        else ctx.telegram.sendMessage(chatID, "Wrong usage => */ask <replace with your question>*", { parse_mode: "Markdown" })
     }
     catch(err){
         console.log(err)
@@ -133,9 +148,9 @@ bot.command('archive', async (ctx) => {
 
 bot.action("push_data", async(ctx) =>{
     const tempMsg = await ctx.telegram.sendMessage(ctx.chat.id, "Processing your push...")
-    let filteredAnswers = tempArray.filter((obj) => obj.chat == ctx.chat.id)
+    let filteredAnswers = tempArray.filter((obj) => obj.user == ctx.from.id)
     let tempObj = filteredAnswers[filteredAnswers.length - 1];
-    tempArray = tempArray.filter((obj) => obj.chat != obj.chat)
+    tempArray = tempArray.filter((obj) => obj.user != ctx.from.id)
     
     if(tempObj){
         let doc = structureDoc("pushRequests")
@@ -155,8 +170,8 @@ bot.action("push_data", async(ctx) =>{
 })
 
 bot.action("reset", (ctx) =>{
-    tempArray = tempArray.filter((obj) => obj.chat != obj.chat)
+    tempArray = tempArray.filter((obj) => obj.user != ctx.from.id)
 })
 
-//bot.launch()
-module.exports = bot
+bot.launch()
+//module.exports = bot
