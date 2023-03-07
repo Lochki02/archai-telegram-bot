2539719d3d10eee9cccca2f3010937d66054607b
IoSonoN <crudocommunityofficial@gmail.com>
2023-03-07 19:50:51 +0100

    fix

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ bot.action("push_data", async (ctx) => {
             })
             let res = await req.json()
 
-            if (!res.failed) ctx.telegram.sendMessage(ctx.from.id, "Congratulation, you've been assigned ticket #" + res.ticket.ticket_id + " for campaign " + res.ticket.campaign)
+            if (!res.failed) ctx.telegram.sendMessage(ctx.from.id, "Congratulation, you've been assigned ticket #" + res.ticket.id + " for campaign " + res.ticket.campaign)
             else ctx.telegram.sendMessage(ctx.from.id, res.error)
         }
     }
